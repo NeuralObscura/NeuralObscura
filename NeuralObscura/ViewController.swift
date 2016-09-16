0d097497d414d5ecd6ea1c08048a9904b5996f74
Paul Bergeron <paul.d.bergeron@gmail.com>
2016-09-15 23:13:07 -0700

    Setup barebones camera loading or photolibrary loading app

--- NeuralObscura/ViewController.swift
+++ NeuralObscura/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func takePicture(_ sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        // If the device has a camera, take a picture, otherwise,
+        // just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+
+        imagePicker.delegate = self
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func takePictureFromLibrary(_ sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+
+        imagePicker.delegate = self
+
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
