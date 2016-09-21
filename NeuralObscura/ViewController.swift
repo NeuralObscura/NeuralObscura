689cb1fa32a50b2d88110a181db6d1e346ee8e25
Paul Bergeron <paul.d.bergeron@gmail.com>
2016-09-20 23:09:01 -0700

    First shader working to make the image VGG compatible

--- NeuralObscura/ViewController.swift
+++ NeuralObscura/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     private var textureLoader : MTKTextureLoader!
     private var commandQueue: MTLCommandQueue!
     var sourceTexture : MTLTexture? = nil
+    let bytesPerPixel: Int = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,6 +74,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
 
     @IBAction func doStyling(_ sender: AnyObject) {
+        // The custom compute kernels for preprocessing the input images.
+        let pipelineRGB: MTLComputePipelineState
+        let pipelineBGR: MTLComputePipelineState
+        let outputImage: MPSImage
+
         var image = imageView.image!.cgImage
         if (image == nil) {
             let ciImage = CIImage(image: imageView.image!)
@@ -86,14 +92,77 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             fatalError("Unexpected error ocurred: \(error.localizedDescription).")
         }
 
-        //TODO: get output image by running model
-        let model = CompositionModel()
+        // Before we pass an image into the network, we need to adjust its RGB
+        // values. This is done with a custom compute kernel. Here we load that
+        // kernel (from Shaders.metal) and set up the compute pipeline.
+        do {
+            let library = device.newDefaultLibrary()!
+            let adjust_mean_rgb = library.makeFunction(name: "adjust_mean_rgb")
+            pipelineRGB = try device.makeComputePipelineState(function: adjust_mean_rgb!)
+
+            let adjust_mean_bgr = library.makeFunction(name: "adjust_mean_bgr")
+            pipelineBGR = try device.makeComputePipelineState(function: adjust_mean_bgr!)
+        } catch {
+            fatalError("Error initializing compute pipeline")
+        }
+
+        let input_id  = MPSImageDescriptor(channelFormat: .float16, width: 224, height: 224, featureChannels: 3)
+
+        autoreleasepool{
+            let commandBuffer = commandQueue.makeCommandBuffer()
+
+            let adjustedMeanImage = MPSTemporaryImage(commandBuffer: commandBuffer, imageDescriptor: input_id)
+
+            let encoder = commandBuffer.makeComputeCommandEncoder()
+            encoder.setComputePipelineState(true ? pipelineBGR : pipelineRGB)
+            encoder.setTexture(sourceTexture, at: 0)
+            encoder.setTexture(adjustedMeanImage.texture, at: 1)
+            let threadsPerGroups = MTLSizeMake(8, 8, 1)
+            let threadGroups = MTLSizeMake(sourceTexture!.width / threadsPerGroups.width,
+                                           adjustedMeanImage.texture.height / threadsPerGroups.height, 1)
+            encoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadsPerGroups)
+            encoder.endEncoding()
+            adjustedMeanImage.readCount -= 1
+
+            // Tell the GPU to start and wait until it's done.
+            commandBuffer.commit()
+            commandBuffer.waitUntilCompleted()
+
+            //TODO: get output image by running model
+            //let model = CompositionModel()
+        }
 
         print("done")
 
-        // Comes out flipped over x axis and oddly colored
-        let ciImage = CIImage(mtlTexture: sourceTexture!)
-        imageView.image = UIImage(ciImage: ciImage!)
+        imageView.image = imageFromTexture(texture: sourceTexture!)
+    }
+
+    func imageFromTexture(texture: MTLTexture) -> UIImage {
+
+        // The total number of bytes of the texture
+        let imageByteCount = texture.width * texture.height * bytesPerPixel
+
+        // The number of bytes for each image row
+        let bytesPerRow = texture.width * bytesPerPixel
+
+        // An empty buffer that will contain the image
+        var src = [UInt8](repeating: 0, count: Int(imageByteCount))
+
+        // Gets the bytes from the texture
+        let region = MTLRegionMake2D(0, 0, texture.width, texture.height)
+        texture.getBytes(&src, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
+
+        // Creates an image context
+        let bitmapInfo = CGBitmapInfo(rawValue: (CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue))
+        let bitsPerComponent = 8
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: &src, width: texture.width, height: texture.height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue);
+
+        // Creates the image from the graphics context
+        let dstImage = context!.makeImage();
+
+        // Creates the final UIImage
+        return UIImage(cgImage: dstImage!, scale: 0.0, orientation: UIImageOrientation.downMirrored)
     }
 
 }
