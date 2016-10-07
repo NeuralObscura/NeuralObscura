6efe571cce6138fcf6e2d6230251f7e998bbcf3d
Teddy Knox <eknox@linkedin.com>
2016-10-06 21:23:43 -0700

    Remove identity encode comment.

--- NeuralObscura/Protocols.swift
+++ NeuralObscura/Protocols.swift
@@ -26,33 +26,3 @@ protocol CommandEncoderDelegate {
     func encode(commandBuffer: MTLCommandBuffer, sourceImage: MPSImage, destinationImage: MPSImage)
     
 }
-
-extension CommandEncoderDelegate {
-    func identity(device: MTLDevice, commandBuffer: MTLCommandBuffer, sourceImage: MPSImage, destinationImage: MPSImage) throws {
-//        let library = device.newDefaultLibrary()!
-//        let identity = library.makeFunction(name: "identity")
-//        let pipelineRGB = try device.makeComputePipelineState(function: identity!)
-//        let pipelineBGR = try device.makeComputePipelineState(function: identity!)
-//        
-//        let identity = library.makeFunction(name: "identity")
-//        pipelineIdentity = try device.makeComputePipelineState(function: identity!)
-//        let encoder = commandBuffer.makeComputeCommandEncoder()
-//        //encoder.setComputePipelineState(true ? pipelineBGR : pipelineRGB)
-//        //encoder.setComputePipelineState(pipelineRGB)
-//        //encoder.setComputePipelineState(pipelineBGR)
-//        encoder.setComputePipelineState(pipelineIdentity)
-//        encoder.setTexture(sourceTexture, at: 0)
-//        //encoder.setTexture(adjustedMeanImage.texture, at: 1)
-//        encoder.setTexture(outputImage.texture, at: 1)
-//        let threadsPerGroups = MTLSizeMake(8, 8, 1)
-//        /*            let threadGroups = MTLSizeMake(sourceTexture!.width / threadsPerGroups.width,
-//         adjustedMeanImage.texture.height / threadsPerGroups.height, 1)*/
-//        let threadGroups = MTLSizeMake(sourceTexture!.width / threadsPerGroups.width,
-//                                       outputImage.texture.height / threadsPerGroups.height, 1)
-//        encoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadsPerGroups)
-//        encoder.endEncoding()
-//        adjustedMeanImage.readCount -= 1
-//
-    }
-}
-
