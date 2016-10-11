93e72d1b9eede813268fabd310c113bb3fa5d834
Teddy Knox <eknox@linkedin.com>
2016-10-11 12:04:19 -0700

    Non-working state of tests.

---
+++ NeuralObscuraTests/CommandEncoderBaseTest.swift
@@ -0,0 +1,35 @@
+
+//
+//  CommandEncoderBaseTest.swift
+//  NeuralObscura
+//
+//  Created by Edward Knox on 10/9/16.
+//  Copyright © 2016 Paul Bergeron. All rights reserved.
+//
+
+import XCTest
+import MetalKit
+import MetalPerformanceShaders
+@testable import NeuralObscura
+
+class CommandEncoderBaseTest: XCTestCase {
+    var device: MTLDevice!
+    var textureLoader: MTKTextureLoader!
+    var commandQueue: MTLCommandQueue!
+    var commandBuffer: MTLCommandBuffer!
+    
+    override func setUp() {
+        super.setUp()
+        device = MTLCreateSystemDefaultDevice()!
+        assert(MPSSupportsMTLDevice(device) == true)
+        textureLoader = MTKTextureLoader(device: device)
+        commandQueue = device.makeCommandQueue()
+        commandBuffer = commandQueue.makeCommandBuffer()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
