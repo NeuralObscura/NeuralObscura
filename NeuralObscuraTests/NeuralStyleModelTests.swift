61b2a20fc3a58ba337cb9b82bc0943d6a3c12a55
Teddy Knox <eknox@linkedin.com>
2017-05-29 18:43:06 -0700

    Add another incremental test.

--- NeuralObscuraTests/NeuralStyleModelTests.swift
+++ NeuralObscuraTests/NeuralStyleModelTests.swift
@@ -11,21 +11,12 @@ import MetalKit
 import MetalPerformanceShaders
 @testable import NeuralObscura
 
-class IncrementalModelGroundTruthTests: CommandEncoderBaseTest {
+class NeuralStyleTests: CommandEncoderBaseTest {
     
     func testStage0() {
         let testUrl = Bundle(for: type(of: self))
             .url(forResource: "tubingen", withExtension: "jpg", subdirectory: "testdata")!
-//        let testImg = UIImage.init(contentsOfFile: testUrl.path)!.toMPSImage(device: device)
-        let options: [String : NSObject] = [
-            MTKTextureLoaderOptionTextureUsage: MTLTextureUsage.shaderRead.rawValue as NSNumber,
-            MTKTextureLoaderOptionAllocateMipmaps: false as NSNumber,
-            MTKTextureLoaderOptionGenerateMipmaps: false as NSNumber,
-            MTKTextureLoaderOptionSRGB: false as NSNumber,
-            MTKTextureLoaderOptionTextureStorageMode: MTLStorageMode.shared.rawValue as NSNumber
-        ]
-        let texture = try! textureLoader.newTexture(withContentsOf: testUrl, options: options)
-        let testImg = MPSImage.init(texture: texture, featureChannels: 4)
+        let testImg = UIImage.init(contentsOfFile: testUrl.path)!.toMPSImage(device: device)
         
         let outImg = BGRAToBRGALayer()
             .chain(MPSImageVariable(testImg))
@@ -37,7 +28,36 @@ class IncrementalModelGroundTruthTests: CommandEncoderBaseTest {
         let expImg = MPSImage.fromNumpy(expUrl)
         
         XCTAssert(outImg.isLossyEqual(image: expImg, precision: -1))
-        print(outImg)
-        print(expImg)
+    }
+    
+    func testStage1() {
+        let testUrl = Bundle(for: type(of: self))
+            .url(forResource: "tubingen", withExtension: "jpg", subdirectory: "testdata")!
+        let testImg = UIImage.init(contentsOfFile: testUrl.path)!.toMPSImage(device: device)
+        
+        let c1_w = FileParameterBuffer(modelName: "composition", rawFileName: "c1_W")
+        let c1_b = FileParameterBuffer(modelName: "composition", rawFileName: "c1_b")
+        
+        let preprocess = BGRAToBRGALayer()
+        let c1 = ConvolutionLayer(
+            kernelSize: 9,
+            channelsIn: 3,
+            channelsOut: 32,
+            w: c1_w,
+            b: c1_b,
+            relu: true,
+            padding: 4,
+            stride: 1)
+        
+        let a = preprocess.chain(MPSImageVariable(testImg))
+        let b = c1.chain(a)
+        let outImg = b.forward(commandBuffer: commandBuffer)
+        execute()
+        
+        let expUrl = Bundle(for: type(of: self))
+            .url(forResource: "inc_gt_1", withExtension: "npy", subdirectory: "testdata")!
+        let expImg = MPSImage.fromNumpy(expUrl)
+        
+        XCTAssert(outImg.isLossyEqual(image: expImg, precision: -1))
     }
 }
